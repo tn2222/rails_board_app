e9e73f669cdd18d836976ca5fd125387b3c6e2b6
takuya nakazawa <>
2020-05-23 12:47:52 +0900

    一旦コミット

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,7 +2,8 @@ class BoardsController < ApplicationController
   before_action :set_target_board, only: %i[show edit update destroy]
 
   def index
-   @boards = Board.page(params[:page])
+   @boards = params[:tag_id].present? ? Tag.find(params[:tag_id]).boards : Board.all
+   @boards = @boards.page(params[:page])
   end
 
   def new
@@ -23,25 +24,34 @@ class BoardsController < ApplicationController
   end
 
   def show
+    @comment = Comment.new(board_id: @board.id)
   end
  
   def edit
   end
 
   def update
-    @board.update(board_params)
-    redirect_to @board
+    board = Board.new(board_params)
+    if board.update(board_params)
+        flash[:notice] = "掲示板を変更しました。"
+        redirect_to board
+    else
+        redirect_to new_board_path, flash: {
+            board: board,
+            error_messages: board.errors.full_messages
+        }
+    end
   end
 
   def destroy
-    @board.delete
+    @board.destroy
 
     redirect_to boards_path, flash: { notice: "「#{@board.title}」の掲示板が削除されました。"}
   end
   private
 
   def board_params
-    params.require(:board).permit(:name, :title, :body)
+    params.require(:board).permit(:name, :title, :body, tag_ids: [])
   end
 
   def set_target_board

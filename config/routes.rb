e2fabf64317eebfe2cc78dbb70c7c9d7ae6848d4
takuya nakazawa <>
2020-05-24 15:53:20 +0900

    ログイン機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  root 'home#index'
-  resources :boards
-  resources :comments, only: %i[create destroy]
-end
+    get 'mypage', to: 'users#me'
+    post 'login', to: 'sessions#create'
+    delete 'logout', to: 'sessions#destroy'
+  
+    root 'home#index'
+    resources :users, only: %i[new create]
+    resources :boards
+    resources :comments, only: %i[create destroy]
+  end
+  
